2679d0efd1b49c6120ec6f6fcfbc39570dc40977
Lucas Ferreira <129918519+LucasAndradeF@users.noreply.github.com>
2023-06-07 22:22:04 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,10 @@
+public class Aluno {
+  
+    public String nomes() {
+        return ("Lucas Ferreira" + "\nDaniel Paiva" + "\nCristiano Ronaldo" + "\nLionel Messi" + "\nNeymar Junior\n"); 
+    } 
+    
+    public String notas() {
+        return ("\n9,5" + "\n10" + "\n10" + "\n8,5" + "\n9,5");
+    }
+}
